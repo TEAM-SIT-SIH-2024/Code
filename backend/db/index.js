5ae3fe5ea2dbdcd0fea5fb534cc98675cdaa57c9
Ranjan N <ranjann2004@gmail.com>
2024-08-31 16:09:57 +0530

    Modified error

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,6 +4,34 @@ mongoose.connect(
   "mongodb+srv://admin:tL8mBWGUkmz2720S@cluster0.scziuv1.mongodb.net/HospitalManagementSystem"
 );
 
+const patientSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  age: { type: Number, required: true },
+  gender: { type: String, required: true },
+  contactInfo: {
+    phone: { type: String, required: true },
+    email: { type: String },
+    address: { type: String, required: true },
+  },
+  emergencyContact: {
+    name: { type: String, required: true },
+    relationship: { type: String, required: true },
+    phone: { type: String, required: true },
+  },
+  medicalHistory: { type: String },
+  admissions: [
+    {
+      admissionDate: { type: Date, default: Date.now },
+      department: { type: String, required: true },
+      doctorInCharge: { type: String, required: true },
+      reasonForAdmission: { type: String, required: true },
+      bed: { type: String, required: true },
+      medications: [{ type: String }],
+      dischargeDate: { type: Date },
+    },
+  ],
+});
+
 const AdminSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
@@ -51,34 +79,6 @@ const CitiesSchema = new mongoose.Schema({
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Admin",
-    },
-  ],
-});
-
-const patientSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-  age: { type: Number, required: true },
-  gender: { type: String, required: true },
-  contactInfo: {
-    phone: { type: String, required: true },
-    email: { type: String },
-    address: { type: String, required: true },
-  },
-  emergencyContact: {
-    name: { type: String, required: true },
-    relationship: { type: String, required: true },
-    phone: { type: String, required: true },
-  },
-  medicalHistory: { type: String },
-  admissions: [
-    {
-      admissionDate: { type: Date, default: Date.now },
-      department: { type: String, required: true },
-      doctorInCharge: { type: String, required: true },
-      reasonForAdmission: { type: String, required: true },
-      bed: { type: String, required: true },
-      medications: [{ type: String }],
-      dischargeDate: { type: Date },
     },
   ],
 });
