b1041e7143a7bfab80fda6e2882e539ace6aae05
vinayagangadhar02 <vinayagangadhar2004@gmail.com>
2024-08-31 14:48:56 +0530

    Home Page Final

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,21 +6,20 @@ function Home() {
     <div>
     <div className="container">
       <nav>
-      
-        <div className="nav__logo">
-        <img className="SIHimg" src="./src/components/SIH.png" id='C' />
-        MediQueue
-        
-
+        <div className="nav__first">
+          <div>
+      <img className="SIHimg" src="./src/components/SIH.png" id='C' />
+      </div>
+        <div className="nav__logo"> MediQueue</div>
         </div>
         <ul className="nav__links">
           <li className="link"><a href="#">Home</a></li>
           <li className="link"><a href="#">About Us</a></li>
           <li className="link"><a href="#">Beds</a></li>
-          <li className="link"><a href="#">Help & Support</a></li>
-        
+          {/* <li className="link"><a href="#">Help & Support</a></li>
+         */}
         </ul>
-        <button className="btn">Sign up</button>
+        <button className="btn">Help & Support</button>
       </nav>
     </div>
     <header>
@@ -37,16 +36,20 @@ function Home() {
       </nav> */}
       <div className="section__container header__container" id="home">
         <div className="header__content">
-          <h1>Providing an Exceptional Patient Experience</h1>
+          <h1>Empowering Your Health with Care and Innovation</h1>
+          <br></br>
           <p>
-            Welcome, where exceptional patient experiences are our priority.
+            {/* Welcome, where exceptional patient experiences are our priority.
             With compassionate care, state-of-the-art facilities, and a
             patient-centered approach, we're dedicated to your well-being. Trust
-            us with your health and experience the difference.
+            us with your health and experience the difference. */}
+             Welcome to a place where your health journey is enhanced by our 
+             advanced facilities and compassionate team. We are dedicated to providing you with the best care possible.
           </p>
+          <br></br>
           <button className="btn">See Services</button>
         </div>
-        <div className="header__form">
+        {/* <div className="header__form">
           <form>
             <h4>Book Now</h4>
             <input type="text" placeholder="First Name" />
@@ -55,7 +58,7 @@ function Home() {
             <input type="text" placeholder="Phone No." />
             <button className="btn form__btn">Book Appointment</button>
           </form>
-        </div>
+        </div> */}
       </div>
     </header>
 
@@ -73,28 +76,25 @@ function Home() {
       <div className="service__grid">
         <div className="service__card">
           <span><i className="ri-microscope-line"></i></span>
-          <h4>Laboratory Test</h4>
+          <h4>OPD Queue</h4>
           <p>
-            Accurate Diagnostics, Swift Results: Experience top-notch Laboratory
-            Testing at our facility.
+          Experience hassle-free OPD services with our streamlined queue management system.
           </p>
           <a href="#">Learn More</a>
         </div>
         <div className="service__card">
           <span><i className="ri-mental-health-line"></i></span>
-          <h4>Health Check</h4>
+          <h4>Bed Availability</h4>
           <p>
-            Our thorough assessments and expert evaluations help you stay
-            proactive about your health.
+          Stay informed with our accurate and timely bed availability tracking, ensuring prompt and efficient care.
           </p>
           <a href="#">Learn More</a>
         </div>
         <div className="service__card">
           <span><i className="ri-hospital-line"></i></span>
-          <h4>General Dentistry</h4>
+          <h4>City Module</h4>
           <p>
-            Experience comprehensive oral care with Dentistry. Trust us to keep
-            your smile healthy and bright.
+          Integrating city-wide healthcare services for seamless access and coordinated patient management across the region.
           </p>
           <a href="#">Learn More</a>
         </div>
@@ -104,71 +104,69 @@ function Home() {
       <div className="about__content">
         <h2 className="section__header">About Us</h2>
         <p>
-          Welcome to our healthcare website, your one-stop destination for
-          reliable and comprehensive health care information. We are committed
-          to promoting wellness and providing valuable resources to empower you
-          on your health journey.
+        Welcome to our healthcare system, your go-to platform for innovative solutions in hospital management and patient care. 
+        We are dedicated to enhancing healthcare efficiency and ensuring that patients receive timely and effective care.
+
+
         </p>
         <p>
-          Explore our extensive collection of expertly written articles and
-          guides covering a wide range of health topics. From understanding
-          common medical conditions to tips for maintaining a healthy lifestyle,
-          our content is designed to educate, inspire, and support you in making
-          informed choices for your health.
+        Explore our cutting-edge technology designed to electronically detect bed occupancy in hospitals.
+ Our system provides real-time updates, allowing healthcare providers to optimize bed management and improve patient flow, 
+ ensuring that every patient receives the attention they need without delay.
         </p>
         <p>
-          Discover practical health tips and lifestyle advice to optimize your
-          physical and mental well-being. We believe that small changes can lead
-          to significant improvements in your quality of life, and we're here to
-          guide you on your path to a healthier and happier you.
+        Stay informed with our advanced solutions for hospital operations. By integrating technology with patient care, 
+we aim to streamline hospital processes, reduce wait times, and enhance the overall patient experience. 
+Trust us to support your healthcare facility in delivering the highest standard of care.
         </p>
       </div>
       <div className="about__image">
-        <img src="assets/about.jpg" alt="about" />
+        <img src="./src/components/about.jpg" alt="about" />
       </div>
     </section>
 
     <section className="section__container why__container" id="blog">
       <div className="why__image">
-        <img src="assets/choose-us.jpg" alt="why choose us" />
+        <img src="./src/components/choose-us.jpg" alt="why choose us" />
       </div>
       <div className="why__content">
         <h2 className="section__header">Why Choose Us</h2>
         <p>
-          With a steadfast commitment to your well-being, our team of highly
-          trained healthcare professionals ensures that you receive nothing
-          short of exceptional patient experiences.
+        Why Choose Us With a focus on innovation and efficiency, our advanced bed occupancy detection system ensures that hospital 
+        resources are managed effectively, providing you with timely and exceptional patient care.
+ 
+
+ 
+
+
         </p>
         <div className="why__grid">
           <span><i className="ri-hand-heart-line"></i></span>
           <div>
-            <h4>Intensive Care</h4>
+            <h4>Optimized Patient Flow</h4>
             <p>
-              Our Intensive Care Unit is equipped with advanced technology and
-              staffed by team of professionals
+            Our system enhances patient flow by accurately detecting bed occupancy, ensuring that each patient receives prompt attention and care.
             </p>
           </div>
           <span><i className="ri-truck-line"></i></span>
           <div>
-            <h4>Free Ambulance Car</h4>
+            <h4>Real-Time Bed Tracking</h4>
             <p>
-              A compassionate initiative to prioritize your health and
-              well-being without any financial burden.
+            We offer real-time bed availability updates, enabling seamless coordination between departments and minimizing patient wait times.
             </p>
           </div>
           <span><i className="ri-hospital-line"></i></span>
           <div>
-            <h4>Medical and Surgical</h4>
+            <h4>Efficient Resource Management </h4>
             <p>
-              Our Medical and Surgical services offer advanced healthcare
-              solutions to address medical needs.
+            Our technology-driven approach ensures that hospital beds are utilized efficiently, allowing healthcare providers to focus on delivering the best medical and surgical care.
             </p>
           </div>
         </div>
       </div>
     </section>
 
-    <section className="section__container doctors__container" id="pages">
+    {/* <section className="section__container doctors__container" id="pages">
       <div className="doctors__header">
         <div className="doctors__header__content">
           <h2 className="section__header">Our Special Doctors</h2>
@@ -223,12 +221,12 @@ function Home() {
           <p>Dermatologist</p>
         </div>
       </div>
-    </section>
+    </section> */}
 
     <footer className="footer">
       <div className="section__container footer__container">
         <div className="footer__col">
-          <h3>Health<span>Care</span></h3>
+          <h3>Medi<span>Queue</span></h3>
           <p>
             We are honored to be a part of your healthcare journey and committed
             to delivering compassionate, personalized, and top-notch care every
